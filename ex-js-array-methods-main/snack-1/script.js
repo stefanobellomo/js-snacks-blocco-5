181572d305d86cb99b603466bda0f085f6c8ee36
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-22 14:19:09 +0200

    snack 1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,12 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+names.forEach(thisame => {
+    console.log(thisame);
+})
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    console.log(name);
+}
