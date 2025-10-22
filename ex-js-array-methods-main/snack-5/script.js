ca92a38a565e8061d8d87590b5a5c5a1614276ea
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-22 15:02:56 +0200

    snack 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,25 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const is_even = nums.filter((even) => {
+    if (even % 2 === 0) {
+        return true
+    } return false
+})
+console.log(is_even);
+
+let even = []
+
+for (let i = 0; i < nums.length; i++) {
+    const numb = nums[i];
+    if (numb % 2 === 0) {
+        console.log(numb);
+    }
+}
+
+nums.forEach((numb) =>{
+    if (numb % 2 === 0) {
+        console.log(numb);
+    }
+})
