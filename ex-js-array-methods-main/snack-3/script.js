5d747583a28f8433b8fe57baf51f9e25a66f54c0
stefanobellomo <stefanobellomo1998@gmail.com>
2025-10-22 14:44:44 +0200

    snack 3

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,18 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+const number = numbers.map((number) => {
+    return number + 1
+});
+console.log(number);
+
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+for (let i = 0; i < numbers.length; i++) {
+    let number = numbers[i] + 1;
+    console.log(number);
+}
+
+numbers.forEach(number => {
+    console.log(number + 1);
+})
